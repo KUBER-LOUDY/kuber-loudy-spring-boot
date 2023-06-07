e07074b824d2151ea3c3fcb67807fe3be75f85d3
yhjune <yhjune09@gmail.com>
2023-06-07 14:00:53 +0900

    🖋 chore : delete  unnecessary files
    properties & package

--- src/main/java/com/kuberloudy/spring/global/config/SecurityConfig.java
+++ src/main/java/com/kuberloudy/spring/global/config/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.kuberloudy.spring.global.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
