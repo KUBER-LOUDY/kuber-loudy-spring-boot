f969896811b64042cca0dcc3c0aa700bf9af4b2e
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-07-25 01:33:43 +0900

    ✨ feat: Method Argument에서 멤버 자동화

---
+++ kl-common/src/main/java/com/kuberloudy/auth/AuthMemberArgumentResolver.java
@@ -0,0 +1,34 @@
+package com.kuberloudy.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class AuthMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthMember.class);
+    }
+
+    @Override
+    public String resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        AuthMember authMemberAnnotation = parameter.getParameterAnnotation(AuthMember.class);
+        assert authMemberAnnotation != null;
+
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getName() == null) {
+            throw  new RuntimeException("Security Context 에 인증 정보가 없습니다.");
+        }
+
+        return authentication.getName();
+    }
+}
