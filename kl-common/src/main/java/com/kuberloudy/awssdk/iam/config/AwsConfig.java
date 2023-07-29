db5b0349b93d71e49f4b1caa68103463a7c0a539
HyoJung Yoon <yhjune09@gmail.com>
2023-07-29 13:21:01 +0900

    ✨ feat: aws config file path 설정

--- kl-common/src/main/java/com/kuberloudy/awssdk/iam/config/AwsConfig.java
+++ kl-common/src/main/java/com/kuberloudy/awssdk/iam/config/AwsConfig.java
@@ -4,18 +4,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.profiles.ProfileFile;
+import software.amazon.awssdk.profiles.ProfileFileSupplier;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.iam.IamClient;
+
+import java.nio.file.Path;
 
 @Configuration
 @RequiredArgsConstructor
 public class AwsConfig {
+
+   Path credentialsFilePath = Path.of("kl-common/src/main/resources/config");
     Region region = Region.AP_NORTHEAST_2;
+   ProfileCredentialsProvider provider = ProfileCredentialsProvider
+            .builder()
+            .profileFile(ProfileFileSupplier.reloadWhenModified(credentialsFilePath, ProfileFile.Type.CREDENTIALS))
+            .build();
+
     @Bean
     public IamClient iamClient() {
         return IamClient.builder()
+                .credentialsProvider(provider)
             .region(region)
-            .credentialsProvider(ProfileCredentialsProvider.create())
             .build();
     }
 }
