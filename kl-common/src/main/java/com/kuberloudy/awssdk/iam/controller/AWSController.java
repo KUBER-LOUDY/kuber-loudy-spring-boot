e24fdba8eb019f79cb61e0e5a988fb704aec9e8c
HyoJung Yoon <yhjune09@gmail.com>
2023-07-30 22:32:07 +0900

    🖋 chore: aws 서비스 분리

--- kl-common/src/main/java/com/kuberloudy/awssdk/iam/controller/AWSController.java
+++ kl-common/src/main/java/com/kuberloudy/awssdk/iam/controller/AWSController.java
@@ -1,36 +1,25 @@
 package com.kuberloudy.awssdk.iam.controller;
 
+import com.kuberloudy.awssdk.iam.service.AwsService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import software.amazon.awssdk.services.iam.IamClient;
 import software.amazon.awssdk.services.sso.SsoClient;
 
-@Slf4j
 @RequiredArgsConstructor
 @RequestMapping(value = "/aws",produces = "application/json; charset=utf8")
 @RestController
 public class AWSController {
 
-    private final SsoClient ssoClient;
-    private final IamClient iamClient;
+    private final AwsService awsService;
+    //Iam 자격 확인
+  /*  @GetMapping("/client")
+    public ResponseEntity<SsoClient> SsoClient(){
+        return new ResponseEntity<SsoClient>(awsService.getSsoClient(), HttpStatus.OK);
+    }*/
 
-    //유저가 선택한 Iam profile에 따른 Client 빌딩
-    @PostMapping("/iamClient")
-    public void setIamClient(){
-    }
-
-    //Iam profile 클라우드 저장소에 저장
-
-    // 저장한 Iam 리스트 반영
-    @GetMapping("/iamusers")
-    public String IamUserList(){
-        String name = iamClient.listUsers().toString();
-        return name;
-
-    }
 
 }
--- kl-common/src/main/java/com/kuberloudy/awssdk/iam/service/AwsService.java
+++ kl-common/src/main/java/com/kuberloudy/awssdk/iam/service/AwsService.java
@@ -1,2 +1,22 @@
-package com.kuberloudy.awssdk.iam.service;public class AwsService {
+package com.kuberloudy.awssdk.iam.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sso.SsoClient;
+import software.amazon.awssdk.services.sso.model.ListAccountRolesRequest;
+import software.amazon.awssdk.services.sso.model.ListAccountRolesResponse;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AwsService {
+
+    private SsoClient ssoClient;
+
+    //계정 자격 증 체크
+  /* public ListAccountRolesRequest getAccountRoles(){
+
+
+   }*/
 }
