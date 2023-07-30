7722461851f73fd3ac4692186d7fea9f30c56a85
HyoJung Yoon <yhjune09@gmail.com>
2023-07-30 21:40:18 +0900

    🔧 fix: domain iam entity 변경

--- kl-domain/src/main/java/com/kuberloudy/domain/iam/entity/Iam.java
+++ kl-domain/src/main/java/com/kuberloudy/domain/iam/entity/Iam.java
@@ -3,6 +3,7 @@ package com.kuberloudy.domain.iam.entity;
 import com.kuberloudy.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.IdGeneratorType;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
@@ -13,11 +14,15 @@ import java.time.LocalDateTime;
 public class Iam {
 
     @Id
-    private String iamId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long iamId;
 
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @Column(nullable = false, length = 200)
+    private String name;
 
     @Column(nullable = false)
     private String iamAccessKey;
