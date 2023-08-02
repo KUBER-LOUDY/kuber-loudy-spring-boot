e1525212c8b0016402fcfaac378a7aa0b1cb4726
HyoJung Yoon <yhjune09@gmail.com>
2023-08-02 19:42:23 +0900

    🔧 fix: AwsService final 수정

--- kl-common/src/main/java/com/kuberloudy/awssdk/iam/service/AwsService.java
+++ kl-common/src/main/java/com/kuberloudy/awssdk/iam/service/AwsService.java
@@ -3,20 +3,23 @@ package com.kuberloudy.awssdk.iam.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.iam.IamClient;
 import software.amazon.awssdk.services.sso.SsoClient;
-import software.amazon.awssdk.services.sso.model.ListAccountRolesRequest;
-import software.amazon.awssdk.services.sso.model.ListAccountRolesResponse;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AwsService {
 
-    private SsoClient ssoClient;
+    private final SsoClient ssoClient;
+    private final IamClient iamClient;
 
-    //계정 자격 증 체크
-  /* public ListAccountRolesRequest getAccountRoles(){
+    public void getssoClient(){
+        log.info(ssoClient.serviceName());
+    }
+    public void IamUserList(){
+        String name = iamClient.listUsers().users().get(0).toString();
+        log.info(name);
+    }
 
-
-   }*/
 }
