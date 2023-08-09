6a00fa03c34469326ecf3923eeedd86936f745bb
yhjune <yhjune09@gmail.com>
2023-08-09 22:56:14 +0900

    🚑️ (fix): Add webconfig CORS filter

--- kl-admin/src/main/java/com/kuberloudy/config/WebConfig.java
+++ kl-admin/src/main/java/com/kuberloudy/config/WebConfig.java
@@ -2,9 +2,14 @@ package com.kuberloudy.config;
 
 import com.kuberloudy.auth.AuthMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,12 +24,23 @@ public class WebConfig implements WebMvcConfigurer {
         argumentResolvers.add(authMemberArgumentResolver);
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("localhost:3000")
-                .allowedMethods("GET","POST","PUT","PATCH")
-                .allowedHeaders("headers")
-                .maxAge(3000);
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://kuber-loudy-front.vercel.app");
+        config.addAllowedHeader("Content-Type");
+        config.addAllowedHeader("charset");
+        config.addAllowedMethod("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
+
+
+
 }
